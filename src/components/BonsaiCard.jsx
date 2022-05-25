4efe517cd45c7a4488b05cbb055ee45e36fab258
potlitel <potlitel@gmail.com>
2022-05-24 22:59:38 -0400

    Se completan todas las imágenes de los tipos de bonsais existentes hasta ahora en el site. Se adicina efecto mouse over sobre el componente BonsaiCard.jsx.

--- src/components/BonsaiCard.jsx
+++ src/components/BonsaiCard.jsx
@@ -1,9 +1,11 @@
 import styles from "../BonsaiCard.module.css";
 export function BonsaiCard({ bonsai }) {
   return(
-    <li className={styles.bonsaiCard}>
-        <img className={styles.bonsaiImage} src={bonsai.imgPath} alt={bonsai.name} width={230} height={345}></img>
-        <div>{bonsai.name}</div>
-    </li>
+    <div className={styles.card}>
+      <li className={styles.bonsaiCard}>
+          <img className={styles.bonsaiImage} src={bonsai.imgPath} alt={bonsai.name} width={230} height={345}></img>
+          <div><h5>{bonsai.name}</h5></div>
+      </li>
+    </div>
   );
 }
