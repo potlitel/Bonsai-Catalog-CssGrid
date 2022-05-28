5126db43adfe1644c4c5b79049824df0c6ce6d3b
potlitel <potlitel@gmail.com>
2022-05-28 16:50:56 -0400

    Se modifica el componente BonsaiDetails.jsx.

--- src/components/BonsaiDetails.jsx
+++ src/components/BonsaiDetails.jsx
@@ -1,5 +1,13 @@
 import { useParams } from "react-router-dom"
+import { Footer } from "./Footer";
+import { Header } from "./Header";
 export function BonsaiDetails() {
     let { id } =useParams();
-    return <div> Hola { id }</div>
+    return (
+        <div>
+            <Header />
+            <div> Hola { id }</div>
+            <Footer />
+        </div>
+    );
 }
